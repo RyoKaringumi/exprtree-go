42dbefc4a39d84d9b6e42a097c6a91e16cb85124
RyoKaringumi <ryo@ezoe.net>
2026-01-06 01:14:33 +0900

    演算子の評価機能を追加: 加算、減算、乗算、除算の式を実装

--- main.go
+++ main.go
@@ -1,5 +1,91 @@
 package main
 
+type ExpressValue interface {
+}
+
+type NumberValue struct {
+	ExpressValue
+	Value float64
+}
+
+type Expression interface {
+	eval() ExpressValue
+}
+
+type AddExpression struct {
+	Expression
+	Left  Expression
+	Right Expression
+}
+
+type SubtractExpression struct {
+	Expression
+	Left  Expression
+	Right Expression
+}
+
+type MultiplyExpression struct {
+	Expression
+	Left  Expression
+	Right Expression
+}
+
+type DivideExpression struct {
+	Expression
+	Left  Expression
+	Right Expression
+}
+
+func (e *AddExpression) eval() ExpressValue {
+	leftVal := e.Left.eval()
+	rightVal := e.Right.eval()
+
+	if leftNum, ok := leftVal.(*NumberValue); ok {
+		if rightNum, ok := rightVal.(*NumberValue); ok {
+			return &NumberValue{Value: leftNum.Value + rightNum.Value}
+		}
+	}
+	return nil
+}
+
+func (e *SubtractExpression) eval() ExpressValue {
+	leftVal := e.Left.eval()
+	rightVal := e.Right.eval()
+
+	if leftNum, ok := leftVal.(*NumberValue); ok {
+		if rightNum, ok := rightVal.(*NumberValue); ok {
+			return &NumberValue{Value: leftNum.Value - rightNum.Value}
+		}
+	}
+	return nil
+}
+
+func (e *MultiplyExpression) eval() ExpressValue {
+	leftVal := e.Left.eval()
+	rightVal := e.Right.eval()
+
+	if leftNum, ok := leftVal.(*NumberValue); ok {
+		if rightNum, ok := rightVal.(*NumberValue); ok {
+			return &NumberValue{Value: leftNum.Value * rightNum.Value}
+		}
+	}
+	return nil
+}
+
+func (e *DivideExpression) eval() ExpressValue {
+	leftVal := e.Left.eval()
+	rightVal := e.Right.eval()
+
+	if leftNum, ok := leftVal.(*NumberValue); ok {
+		if rightNum, ok := rightVal.(*NumberValue); ok {
+			if rightNum.Value != 0 {
+				return &NumberValue{Value: leftNum.Value / rightNum.Value}
+			}
+		}
+	}
+	return nil
+}
+
 func main() {
 	// This is a placeholder for the main function.
 }
